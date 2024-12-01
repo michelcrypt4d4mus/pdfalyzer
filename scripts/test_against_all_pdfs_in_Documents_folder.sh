c66f90de0f9d5d2da7679d17b563cb3a28f8d3e8
Pierre-Gronau-ndaal <72132223+Pierre-Gronau-ndaal@users.noreply.github.com>
2024-12-01 11:55:08 +0100

    Update test_against_all_pdfs_in_Documents_folder.sh

--- scripts/test_against_all_pdfs_in_Documents_folder.sh
+++ scripts/test_against_all_pdfs_in_Documents_folder.sh
@@ -1,36 +1,41 @@
-#!/bin/bash
-SCRIPT_PATH=$(dirname -- "$(readlink -f -- "$0";)";)
-source "$SCRIPT_PATH/lib/project_paths.sh"
+#!/usr/bin/env bash
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+SCRIPT_PATH="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
+readonly SCRIPT_PATH
+source "${SCRIPT_PATH}/lib/project_paths.sh"
 
 # Exporting makes these available to the 'find -exec bash -c' invocation
 export PDFALYZER_EXECUTABLE
-export SUCCESS_LOG=log/successfully_parsed.txt
-export FAILURE_LOG=log/failed_to_parse.txt
-
+export SUCCESS_LOG="log/successfully_parsed.txt"
+export FAILURE_LOG="log/failed_to_parse.txt"
 
 pdfalyze_doc() {
-    pdf_full_path="$(readlink -f "$1")"
-    pdf_basename=`basename "$pdf_full_path"`
+    local pdf_full_path
+    local pdf_basename
+    pdf_full_path="$(readlink -f "${1}")"
+    pdf_basename="$(basename "${pdf_full_path}")"
 
-    if [[ $pdf_basename =~ postgresql.* ]]; then
-        echo "Skipping '$pdf_basename'..."  # Postgres PDF takes forever to process
+    if [[ "${pdf_basename}" =~ postgresql.* ]]; then
+        printf "Skipping '%s'...\n" "${pdf_basename}"  # Postgres PDF takes forever to process
         return
     fi
 
-    cmd="$PDFALYZER_EXECUTABLE -f -r -t \"$pdf_full_path\""
-    echo -e "\nCommand to run: $cmd"
+    printf "\nCommand to run: %s -f -r -t \"%s\"\n" "${PDFALYZER_EXECUTABLE}" "${pdf_full_path}"
 
-    eval $cmd
-
-    if [ $? -eq 0 ]; then
-        echo "$pdf_full_path" >> "$SUCCESS_LOG"
+    if "${PDFALYZER_EXECUTABLE}" -f -r -t "${pdf_full_path}"; then
+        echo "${pdf_full_path}" >> "${SUCCESS_LOG}"
     else
-        echo "$pdf_full_path" >> "$FAILURE_LOG"
+        echo "${pdf_full_path}" >> "${FAILURE_LOG}"
     fi
 }
 
 export -f pdfalyze_doc
 
-rm "$SUCCESS_LOG" 2>/dev/null
-rm "$FAILURE_LOG" 2>/dev/null
-find ~/Documents/ -iname "*.pdf" -type f -exec bash -c 'pdfalyze_doc "{}"' \;
+rm -f -v "${SUCCESS_LOG}" 2>/dev/null
+rm -f -v "${FAILURE_LOG}" 2>/dev/null
+
+find "${HOME}/Documents/" -iname "*.pdf" -type f -print0 | xargs -0 -I {} bash -c 'pdfalyze_doc "$@"' _ {}
