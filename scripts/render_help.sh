47d8f9e4cc3bedc147cbca7fd52744142ea5f153
Pierre-Gronau-ndaal <72132223+Pierre-Gronau-ndaal@users.noreply.github.com>
2024-12-01 11:15:39 +0100

    Update render_help.sh

--- scripts/render_help.sh
+++ scripts/render_help.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-# Render the help to an SVG; render that SVG to a .png image
-RENDER_HELP_FORMAT=png RENDER_HELP_OUTPUT_DIR=doc/svgs/rendered_images/ pdfalyze --help
+#!/usr/bin/env bash
+set -o errtrace
+set -o nounset
+set -o pipefail
+RENDER_HELP_FORMAT="png" RENDER_HELP_OUTPUT_DIR="doc/svgs/rendered_images/" pdfalyze --help
