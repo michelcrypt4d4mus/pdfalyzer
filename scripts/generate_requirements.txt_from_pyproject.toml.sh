5bb69a803627270fbc19121fdefa04e0655cc8e4
vPierre <72132223+Pierre-Gronau-ndaal@users.noreply.github.com>
2024-12-13 15:15:34 +0100

    Update generate_requirements.txt_from_pyproject.toml.sh

--- scripts/generate_requirements.txt_from_pyproject.toml.sh
+++ scripts/generate_requirements.txt_from_pyproject.toml.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
 # poetry claims to be able to do this but I've had some issues crossing platform with the
 # generated requirements, so mileage may vary.
 
