2eb9936f444f63d60b7cb06afe02e726cddee36b
vPierre <72132223+Pierre-Gronau-ndaal@users.noreply.github.com>
2024-12-13 15:34:18 +0100

    Update test_against_all_pdfs_in_Documents_folder.sh

--- scripts/test_against_all_pdfs_in_Documents_folder.sh
+++ scripts/test_against_all_pdfs_in_Documents_folder.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
-SCRIPT_PATH=$(dirname -- "$(readlink -f -- "$0";)";)
-source "$SCRIPT_PATH/lib/project_paths.sh"
+SCRIPT_PATH="$(dirname -- "$(readlink -f -- "$0")")"
+readonly SCRIPT_PATH
+
+source "${SCRIPT_PATH}/lib/project_paths.sh"
 
 # Exporting makes these available to the 'find -exec bash -c' invocation
 export PDFALYZER_EXECUTABLE
-export SUCCESS_LOG=log/successfully_parsed.txt
-export FAILURE_LOG=log/failed_to_parse.txt
-
+export SUCCESS_LOG="log/successfully_parsed.txt"
+export FAILURE_LOG="log/failed_to_parse.txt"
 
 pdfalyze_doc() {
-    pdf_full_path="$(readlink -f "$1")"
-    pdf_basename=`basename "$pdf_full_path"`
-
-    if [[ $pdf_basename =~ postgresql.* ]]; then
-        echo "Skipping '$pdf_basename'..."  # Postgres PDF takes forever to process
+    local pdf_full_path
+    pdf_full_path="$(readlink -f "${1}")"
+    readonly pdf_full_path
+    local pdf_basename
+    pdf_basename="$(basename "${pdf_full_path}")"
+    readonly pdf_basename
+    
+    if [[ "${pdf_basename}" =~ postgresql.* ]]; then
+        printf "Info: Skipping '%s'...\n" "${pdf_basename}"  # Postgres PDF takes forever to process
         return
     fi
 
-    cmd="$PDFALYZER_EXECUTABLE -f -r -t \"$pdf_full_path\""
-    echo -e "\nCommand to run: $cmd"
+    local cmd
+    cmd="${PDFALYZER_EXECUTABLE} -f -r -t \"${pdf_full_path}\""
+    printf "\nInfo: Command to run: %s\n" "${cmd}"
 
-    eval $cmd
+    eval "${cmd}"
 
-    if [ $? -eq 0 ]; then
-        echo "$pdf_full_path" >> "$SUCCESS_LOG"
+    if [[ $? -eq 0 ]]; then
+        printf "%s\n" "${pdf_full_path}" >> "${SUCCESS_LOG}"
     else
-        echo "$pdf_full_path" >> "$FAILURE_LOG"
+        printf "%s\n" "${pdf_full_path}" >> "${FAILURE_LOG}"
     fi
 }
 
