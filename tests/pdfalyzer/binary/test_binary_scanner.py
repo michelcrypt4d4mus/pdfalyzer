69da17459ac285cf3923b2bd0453fa15d2e998a4
ashariyar <ashariyar@users.noreply.github.com>
2025-06-29 14:42:10 -0400

    Add tolerance to some of the unit tests

--- tests/pdfalyzer/binary/test_binary_scanner.py
+++ tests/pdfalyzer/binary/test_binary_scanner.py
@@ -3,19 +3,21 @@ import pytest
 
 @pytest.mark.slow
 def test_quote_extraction_methods(font_info):
-    _check_matches(font_info.binary_scanner.extract_backtick_quoted_bytes, 163, 52840)
+    _check_matches(font_info.binary_scanner.extract_backtick_quoted_bytes, 163, 52840, 2)
 
 
 @pytest.mark.slow
 def test_front_slash_quoted_bytes_extraction(font_info):
-    _check_matches(font_info.binary_scanner.extract_front_slash_quoted_bytes, 756, 167814)
+    _check_matches(font_info.binary_scanner.extract_front_slash_quoted_bytes, 756, 167814, 1)
 
 
 def test_extract_guillemet(font_info):
     _check_matches(font_info.binary_scanner.extract_guillemet_quoted_bytes, 59, 78763)
 
 
-def _check_matches(match_iterator, expected_matches: int, expected_bytes: int) -> None:
+def _check_matches(match_iterator, expected_matches: int, expected_bytes: int, tolerance_pct: int = 0) -> None:
+    num_matches_tolerance = expected_matches * (tolerance_pct / 100)
+    num_bytes_tolerance = expected_bytes * (tolerance_pct / 100)
     quoted_bytes_found = 0
     quoted_sections_found = 0
 
@@ -24,5 +26,8 @@ def _check_matches(match_iterator, expected_matches: int, expected_bytes: int) -
         quoted_sections_found += 1
 
     print(f"sections: {quoted_sections_found}, bytes: {quoted_bytes_found}")
-    assert quoted_sections_found == expected_matches
-    assert quoted_bytes_found == expected_bytes
+    assert quoted_sections_found >= (expected_matches - num_matches_tolerance)
+    assert quoted_sections_found <= (expected_matches + num_matches_tolerance)
+
+    assert quoted_bytes_found >= (expected_bytes - num_bytes_tolerance)
+    assert quoted_bytes_found <= (expected_bytes + num_bytes_tolerance)
