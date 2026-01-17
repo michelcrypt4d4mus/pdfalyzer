759c58b573520c9069ee1c1566026e76b8f23f16
ashariyar <ashariyar@users.noreply.github.com>
2026-01-17 10:12:43 -0500

    test

---
+++ tests/test_font_info.py
@@ -0,0 +1,5 @@
+
+def test_font_extraction(attachment_pdf_pdfalyzer):
+    assert len(attachment_pdf_pdfalyzer.font_infos) == 3
+    font1 = attachment_pdf_pdfalyzer.font_infos[0]
+    assert font1.first_and_last_char == [67, 122]
