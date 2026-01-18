d9a32f2aadec3fd8b3324627ab6d7623ce4665e3
ashariyar <ashariyar@users.noreply.github.com>
2026-01-17 20:23:10 -0500

    imports

--- pdfalyzer/font_info.py
+++ pdfalyzer/font_info.py
@@ -17,8 +17,7 @@ from pdfalyzer.binary.binary_scanner import BinaryScanner
 from pdfalyzer.output.character_mapping import print_character_mapping, print_prepared_charmap
 from pdfalyzer.output.layout import print_section_subheader, subheading_width
 from pdfalyzer.output.styles.node_colors import get_class_style, get_label_style
-from pdfalyzer.util.adobe_strings import (FONT, FONT_DESCRIPTOR, FONT_FILE, FONT_LENGTHS, RESOURCES,
-     SUBTYPE, TO_UNICODE, TYPE, W, WIDTHS)
+from pdfalyzer.util.adobe_strings import FONT, FONT_FILE, FONT_LENGTHS, RESOURCES, TO_UNICODE, W, WIDTHS
 
 FONT_SECTION_PREVIEW_LEN = 30
 MAX_REPR_STR_LEN = 20
