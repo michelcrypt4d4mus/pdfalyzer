9d8d069f6a1af699bd041fe20c0fb9e8e1e25f8c
ashariyar <ashariyar@users.noreply.github.com>
2026-01-19 20:33:20 -0500

    log

--- pdfalyzer/decorators/pdf_tree_verifier.py
+++ pdfalyzer/decorators/pdf_tree_verifier.py
@@ -67,6 +67,8 @@ class PdfTreeVerifier:
         if node_ids_without_parents:
             node_id_to_child_count = {n.idnum: f"has {len(n.children)} children" for n in nodes_without_parents}
             log.warning(f"These node IDs were parsed but have no parent:\n{node_id_to_child_count}\n")
+        elif notable_missing_node_ids:
+            log.warning(f"None of the missing nodes were enountered while walking the tree.", extra={"highlighter": None})
 
     def notable_missing_node_ids(self) -> list[int]:
         """Missing idnums that aren't NullObject, NumberObject, etc."""
