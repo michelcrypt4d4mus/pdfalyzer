e98b998dd1ec9f4108695a575d72d0d54acdaa52
ashariyar <ashariyar@users.noreply.github.com>
2026-01-20 04:42:08 -0500

    test

--- tests/test_pdfalyzer.py
+++ tests/test_pdfalyzer.py
@@ -6,8 +6,11 @@ def test_is_in_tree(analyzing_malicious_pdfalyzer, page_node):
     assert analyzing_malicious_pdfalyzer.is_in_tree(page_node)
 
 
-@pytest.mark.slow
-def test_unplaced_nodes(SF424_page2_pdfalyzer, test_sweep_indirect_references_nullobject_exception_pdfalyzer):
+def test_unplaced_nodes(SF424_page2_pdfalyzer, form_evince_pdfalyzer):
     assert len(SF424_page2_pdfalyzer.missing_node_ids()) == 0
-    # This is very slow (4 minutes!)
+    assert len(form_evince_pdfalyzer.missing_node_ids()) == 11
+
+# This is very slow (4 minutes!)
+@pytest.mark.slow
+def test_slow_unplaced_nodes(test_sweep_indirect_references_nullobject_exception_pdfalyzer):
     assert len(test_sweep_indirect_references_nullobject_exception_pdfalyzer.missing_node_ids()) == 1
