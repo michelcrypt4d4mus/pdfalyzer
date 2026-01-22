74245f0e855b23a3ceefd9acb9d756316c4f83fe
ashariyar <ashariyar@users.noreply.github.com>
2026-01-21 23:41:36 -0500

    fixt ests

--- tests/pdfalyzer/test_pdf_walker.py
+++ tests/pdfalyzer/test_pdf_walker.py
@@ -1,3 +1,4 @@
+"""Use pdf-parser.py to validate our attempt to walk all the nodes in a PDF."""
 from pdfalyzer.util.pdf_parser_manager import PdfParserManager
 
 
@@ -6,17 +7,14 @@ class TestPdfalyzer:
         struct_elem_node = analyzing_malicious_pdfalyzer.find_node_by_idnum(120)
         assert struct_elem_node.parent.idnum == 119
 
-    def test_all_nodes_in_tree(self, analyzing_malicious_pdfalyzer, analyzing_malicious_pdf_path):
-        for object_id in PdfParserManager(analyzing_malicious_pdf_path).object_ids:
+    def test_all_nodes_in_tree(self, analyzing_malicious_pdfalyzer, pdf_parser_manager_args):
+        for object_id in PdfParserManager(pdf_parser_manager_args).object_ids:
             if object_id == 71:
-                # 71 is the ID of the object stream holding many of the /StructElem
-                continue
+                continue  # 71 is the ID of the object stream holding many of the /StructElem
             elif object_id == 67:
-                # 67 is an object without any references or data
-                continue
+                continue  # 67 is an object without any references or data
             elif object_id == 426:
-                # 426 is a Cross-reference stream containing the same info as the trailer
-                continue
+                continue  # 426 is a Cross-reference stream containing the same info as the trailer
 
             node = analyzing_malicious_pdfalyzer.find_node_by_idnum(object_id)
             assert node is not None, f"Expected {object_id} to appear in tree."
