e72ca7359fae0a73211048eb004ccf1e6ab98f4b
ashariyar <ashariyar@users.noreply.github.com>
2026-01-26 22:44:13 -0500

    fixtures in right place

--- tests/pdfalyzer/conftest.py
+++ tests/pdfalyzer/conftest.py
@@ -6,3 +6,14 @@ import pytest
 @pytest.fixture
 def pdf_parser_manager_args(analyzing_malicious_pdf_path, tmp_dir) -> Namespace:
     return Namespace(file_to_scan_path=analyzing_malicious_pdf_path, output_dir=tmp_dir)
+
+
+# /Page and /Pages nodes
+@pytest.fixture(scope="session")
+def page_node(analyzing_malicious_pdfalyzer):
+    return analyzing_malicious_pdfalyzer.find_node_by_idnum(3)
+
+
+@pytest.fixture(scope="session")
+def pages_node(analyzing_malicious_pdfalyzer):
+    return analyzing_malicious_pdfalyzer.find_node_by_idnum(2)
