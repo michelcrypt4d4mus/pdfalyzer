f10e7855d58ba18a1fea4f82bddd814c10abf8cf
ashariyar <ashariyar@users.noreply.github.com>
2026-01-27 03:11:07 -0500

    CLI tool tests on windows

--- tests/cli_tools/test_extract_pdf_pages.py
+++ tests/cli_tools/test_extract_pdf_pages.py
@@ -21,8 +21,9 @@ def extracted_pdf_path(multipage_pdf_path, tmp_dir):
         extracted_path.unlink()
 
 
-def test_extract_pdf_pages(extracted_pdf_path, multipage_pdf_path, tmp_dir):
-    cmd_stdout = check_output(['extract_pdf_pages', '-r', '2', '-d', tmp_dir, multipage_pdf_path], env=environ).decode()
+def test_extract_pdf_pages(extracted_pdf_path, multipage_pdf_path, script_cmd_prefix, tmp_dir):
+    cmd = script_cmd_prefix + ['extract_pdf_pages', '-r', '2', '-d', tmp_dir, multipage_pdf_path]
+    cmd_stdout = check_output(cmd, env=environ).decode()
     assert "__page_2.pdf" in cmd_stdout
     assert extracted_pdf_path.exists()
     text = check_output(['extract_pdf_text', extracted_pdf_path], env=environ).decode()
