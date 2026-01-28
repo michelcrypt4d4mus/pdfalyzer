b652ba123f07336f70e8699e7d0bb5c895755827
ashariyar <ashariyar@users.noreply.github.com>
2026-01-28 17:59:15 -0500

    put back page_node fixture

--- tests/pdfalyzer/decorators/test_pdf_tree_node.py
+++ tests/pdfalyzer/decorators/test_pdf_tree_node.py
@@ -1,3 +1,11 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def pages_node(analyzing_malicious_pdfalyzer):
+    return analyzing_malicious_pdfalyzer.find_node_by_idnum(2)
+
+
 def test_pdf_node_address(analyzing_malicious_pdfalyzer):
     node41 = analyzing_malicious_pdfalyzer.find_node_by_idnum(41)
     assert node41.tree_address() == '/Root/StructTreeRoot/K[0]/K[24]/K[1]/K[3]/K[0]/K[0]/K[1]/K[0]/Obj'
