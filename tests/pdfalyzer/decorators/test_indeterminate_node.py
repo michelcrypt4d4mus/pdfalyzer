7a3b77f9fad3065c8436828fd0cb6c13a7521ab2
ashariyar <ashariyar@users.noreply.github.com>
2026-01-28 17:56:29 -0500

    _find_common_ancestor_among_nodes() is a static method

--- tests/pdfalyzer/decorators/test_indeterminate_node.py
+++ tests/pdfalyzer/decorators/test_indeterminate_node.py
@@ -1,6 +1,6 @@
 from pdfalyzer.decorators.indeterminate_node import _find_common_ancestor_among_nodes
 
 
-def test_find_common_ancestor_among_nodes(page_node, analyzing_malicious_pdfalyzer):
+def test_find_common_ancestor_among_nodes(analyzing_malicious_pdfalyzer):
     nodes = [analyzing_malicious_pdfalyzer.find_node_by_idnum(id) for id in [2, 4, 12, 417]]
     assert _find_common_ancestor_among_nodes(nodes) == nodes[0]
